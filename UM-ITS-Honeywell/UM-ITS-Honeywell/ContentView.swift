89e2a0d3c9a69e40a60cea28969771f4c7e14713
Kiran Menon <43457679+k-menon@users.noreply.github.com>
2020-02-10 12:23:17 -0500

    Add files via upload
    Cannot seem to get project to recognize captuvo library

---
+++ UM-ITS-Honeywell/UM-ITS-Honeywell/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  UM-ITS-Honeywell
+//
+//  Created by UM-Support on 2/7/20.
+//  Copyright © 2020 UM-Support. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
